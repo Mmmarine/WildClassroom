9d8caddad99d75fbd2b3f07480647dfb082fceff
Mmmarine <53342370+Mmmarine@users.noreply.github.com>
2019-10-01 13:41:08 +0200

    Update Classroom.java

--- Classroom.java
+++ Classroom.java
@@ -3,7 +3,6 @@ class Classroom{
     public static void main(String []args){
         
         Wilder un = new Wilder("Marine", false);
-        un.whoAmI();
         System.out.println (un.whoAmI());
     }
 }
